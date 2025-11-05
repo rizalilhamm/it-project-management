725600e3b40eb98a215c1630febc44e4b0e2ffd1
Rizal Ilham <rzlilhm09@gmail.com>
2025-11-05 11:58:24 +0700

    create project:

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/projectsController');
-const { authMiddleware, requireRole } = require('../middleware/auth');
+// const { authMiddleware, requireRole } = require('../middleware/auth');
 
-router.post('/', authMiddleware, requireRole('supervisor'), ctrl.create);
+router.post('/create', ctrl.create);
 
 module.exports = router;
